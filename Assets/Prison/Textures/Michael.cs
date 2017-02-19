5a796b3d39f4c38be0cf5e94ee8f42b56ceef174
Куприянов Кирилл Игоревич <kikupriyanov@edu.hse.ru>
2017-02-19 19:01:55 +0300

    add prison cell

---
+++ Assets/Prison/Textures/Michael.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Michael : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        moveSpeed = 1F;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // print(Input.GetAxis("Horizontal"));
+
+        //transform.Translate(0.1F * Time.deltaTime, 1F, 1F);
+
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime,
+            0,
+            moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
