c2c53a3b3eaede689a8957c54113600875e48e33
Куприянов Кирилл Игоревич <kikupriyanov@edu.hse.ru>
2017-05-02 02:06:48 +0300

    add transit scene

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+
+    public GameObject door;
+    private bool opened;
+
+    // Use this for initialization
+    void Start()
+    {
+        opened = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (!opened)
+        {
+            door.GetComponent<Animation>().Play();
+            opened = true;
+            Debug.Log("opened");
+        }
+    }
+}
