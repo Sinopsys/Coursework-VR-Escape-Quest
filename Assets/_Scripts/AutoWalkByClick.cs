72b966251df1043b444897e865c1dc71f14c6bfd
Куприянов Кирилл Игоревич <kikupriyanov@edu.hse.ru>
2017-05-02 16:30:51 +0300

    complete menu+transit scenes

---
+++ Assets/_Scripts/AutoWalkByClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class AutoWalkByClick : MonoBehaviour
+{
+    public float speed = 10F;
+    public bool moveForward;
+    private CharacterController controller;
+    private GvrViewer gvrViewer;
+    private Transform vrHead;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        gvrViewer = transform.GetChild(0).GetComponent<GvrViewer>();
+        vrHead = Camera.main.transform;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+            moveForward = !moveForward;
+
+        if (moveForward)
+        {
+            Vector3 forward = vrHead.TransformDirection(Vector3.forward);
+            controller.SimpleMove(forward * speed);
+        }
+    }
+}
---
+++ Assets/_Scripts/WalkByLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkByLook : MonoBehaviour
+{
+
+    public float angle = 30F;
+    public float speed = 5F;
+    private bool moveForward;
+    private CharacterController cc;
+    public Transform vrCam;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveForward = (vrCam.eulerAngles.x >= angle && vrCam.eulerAngles.x <= 90F);
+        if (moveForward)
+        {
+            Vector3 forward = vrCam.TransformDirection(Vector3.forward);
+            cc.SimpleMove(forward * speed);
+        }
+    }
+}
